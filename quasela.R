a6c9141a43e67573b06aeffe2d4e70df84839d24
SanCDV <sanatiel.carreiro@unemat.br>
2022-12-07 21:27:29 -0400

    tentativa

---
+++ quasela.R
@@ -0,0 +1,8 @@
+https://github.com/SanCDV/pesquisa.git
+echo "# teste" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin git@github.com:SanCDV/teste.git
+git push -u origin a Principal
