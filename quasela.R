932ba01b5e3d82081dfb86718b8b24fe40e0567b
SanCDV <103159384+SanCDV@users.noreply.github.com>
2022-12-07 21:46:38 -0400

    Update quasela.R

--- quasela.R
+++ quasela.R
@@ -5,4 +5,18 @@ git add README.md
 git commit -m "first commit" 
 git branch -M main 
 git remote add origin git@github.com:SanCDV/teste.git
-git push -u origin a Principal
+git push -u origin a Principal b
+nugbvjbk
+hjv, hvmn
+jnanklsnk h
+.,nnamdn d
+ m ,m, nms
+, dm dnavdku
+q
+gd
+0
+
+
+
+dçsodihalkjjdcwgdb;bvçg;jd
+wdkbçdh j'''g''hkg.'gy'fyj"KHGYFghvvhgbnbgkduhqbdjbhdsdad
